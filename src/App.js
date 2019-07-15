e1e0957607f9eccf67b2d2aad61ca59505001251
Your Name <you@example.com>
2019-07-15 18:05:08 -0400

    rendered the state values from hooks

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [LionScoreOn,setLionScoreOn] = useState(true);
-  const [TigerScoreOn, setTigerScoreOn] = useState(true);
+  const [LionScoreOn,setLionScoreOn] = useState(45);
+  const [TigerScoreOn, setTigerScoreOn] = useState(34);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -15,13 +15,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+            <div className="home__score">{LionScoreOn}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{TigerScoreOn}</div>
           </div>
         </div>
         <BottomRow />
@@ -42,3 +41,5 @@ function App() {
 }
 
 export default App;
+
+

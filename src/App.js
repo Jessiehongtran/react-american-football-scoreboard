03914123502561edea3639f42e09f39091b43fcf
Your Name <you@example.com>
2019-07-15 18:10:24 -0400

    added onClick handler to buttons

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick = {()=> setLionScoreOn(LionScoreOn+7)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick = {()=> setLionScoreOn(LionScoreOn+3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick = {()=> setTigerScoreOn(TigerScoreOn+7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick = {()=> setTigerScoreOn(TigerScoreOn+3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
